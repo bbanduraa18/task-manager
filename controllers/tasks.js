1616fa4c30564ec0124975d5684b39cdab477e26
Bandura Andrii <bbanduraa18@gmail.com>
2022-03-10 21:40:13 +0200

    create and implement empty methods to each route

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,27 @@
 const getAllTasks = (req, res) => {
-	res.send('All items');
+	res.send('get all items');
+};
+
+const createTask = (req, res) => {
+	res.send('create new task');
+};
+
+const getOneTask = (req, res) => {
+	res.send('get one task');
+};
+
+const updateTask = (req, res) => {
+	res.send('update task');
+};
+
+const deleteTask = (req, res) => {
+	res.send('delete task');
 };
 
 module.exports = {
 	getAllTasks,
+	createTask,
+	getOneTask,
+	updateTask,
+	deleteTask
 }
