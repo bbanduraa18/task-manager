250b6657cca74df6eae301a0ca5e5559a00ffc86
Bandura Andrii <bbanduraa18@gmail.com>
2022-03-13 21:15:38 +0200

    refactor method to start the server and connect to DB

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('./db/connect')
+const connectDB = require('./db/connect');
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
@@ -6,6 +6,15 @@ const tasks = require('./routes/tasks');
 app.use(express.json());
 app.use('/api/v1/tasks', tasks);
 
-app.listen(5000, () => {
-	console.log('Server is running on port 5000')
-});
+const start = async () => {
+	try {
+		await connectDB();
+		app.listen(5000, () => {
+			console.log('Server is running on port 5000');
+		});
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+start();
--- db/connect.js
+++ db/connect.js
@@ -1,13 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://bandura:Galya228322@nodeexpressprojects.10gdg.mongodb.net/Task-Manager?retryWrites=true&w=majority'
+const connectionString = 'mongodb+srv://bandura:Galya228322@nodeexpressprojects.10gdg.mongodb.net/Task-Manager?retryWrites=true&w=majority';
 
-mongoose.connect(connectionString,
-	{
-		useNewUrlParser: true,
-		useCreateIndex: true,
-		useFindAndModify: false,
-		useUnifiedTopology: true
-	})
-	.then(() => console.log('Connected to the DB'))
-	.catch((err) => console.log(err))
+const connectDB = (url) => {
+	return mongoose.connect(connectionString,
+		{
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true
+		})
+};
+
+module.exports = connectDB;
