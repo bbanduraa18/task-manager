8aa521d974413efde5117a4a652fb94a0b1b6dac
Bandura Andrii <bbanduraa18@gmail.com>
2022-03-13 21:07:56 +0200

    refactor connection to db to fix bugs

--- db/connect.js
+++ db/connect.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose')
 
 const connectionString = 'mongodb+srv://bandura:Galya228322@nodeexpressprojects.10gdg.mongodb.net/Task-Manager?retryWrites=true&w=majority'
 
-mongoose.connect(connectionString)
+mongoose.connect(connectionString,
+	{
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+		useUnifiedTopology: true
+	})
 	.then(() => console.log('Connected to the DB'))
 	.catch((err) => console.log(err))
